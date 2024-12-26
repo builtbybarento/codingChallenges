5e4b16c483a7c1e531364e21f08c0626f5e08ecc
Barento Muhedin <awesomemoi@hotmail.com>
2024-12-25 20:55:46 -0500

    New leetcode solution "2619. Array Prototype Last"

--- Leetcode/2629. Function Composition.js
+++ Leetcode/2629. Function Composition.js
@@ -1,4 +1,4 @@
-// 2629. Function Composition - easy
+// 2629. Function Composition - Easy
 
 // Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
 
