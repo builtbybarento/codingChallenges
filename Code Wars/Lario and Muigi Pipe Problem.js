ab23f20f40e93f7a8d6b5f0b9340a702924953dd
Barento Muhedin <awesomemoi@hotmail.com>
2024-12-31 11:03:13 -0500

    Made Lario and Muigi problem prettier with the Prettier extension

--- Code Wars/Lario and Muigi Pipe Problem.js
+++ Code Wars/Lario and Muigi Pipe Problem.js
@@ -16,10 +16,10 @@
 
 // Solution:
 
-function pipeFix(numbers){
-  let arr = []
-  for(let i = Math.min(...numbers); i<= Math.max(...numbers); i++){
-    arr.push(i)
+function pipeFix(numbers) {
+  let arr = [];
+  for (let i = Math.min(...numbers); i <= Math.max(...numbers); i++) {
+    arr.push(i);
   }
-  return arr
+  return arr;
 }
