ae420bb46ee9f8f0025be3ae1b185147ac3ca1c5
Barento Muhedin <awesomemoi@hotmail.com>
2024-12-31 11:00:50 -0500

    Replaced wrong code with my actual code

--- Code Wars/Lario and Muigi Pipe Problem.js
+++ Code Wars/Lario and Muigi Pipe Problem.js
@@ -17,14 +17,9 @@
 // Solution:
 
 function pipeFix(numbers){
-    var min = numbers[0];
-    var max = numbers[numbers.length - 1];
-    var array = [];
-    
-    for(var i = min; i<=max; i++)
-    {
-      array.push(i);
-    }
-    
-    return array;
+  let arr = []
+  for(let i = Math.min(...numbers); i<= Math.max(...numbers); i++){
+    arr.push(i)
   }
+  return arr
+}
