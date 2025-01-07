01645d3bcb2cc56b413be3705675dd7dd3dc36ef
Barento Muhedin <awesomemoi@hotmail.com>
2025-01-06 21:38:00 -0500

    New kata "Function 2 - squaring an argument"

---
+++ Code Wars/ Function 2 - squaring an argument.js
@@ -0,0 +1,9 @@
+//  8 kyu - Function 2 - squaring an argument
+
+//  Description - 
+
+//  Now you have to write a function that takes an argument and returns the square of it.
+
+// Solution: 
+
+const square = (num) => num ** 2 
