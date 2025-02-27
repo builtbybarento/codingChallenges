d47772a8e7c3a0866d75cafb2e4510f5da29e733
Barento Muhedin <awesomemoi@hotmail.com>
2025-02-27 16:37:28 -0500

    New leetcode solution "100. Same Tree"

---
+++ Leetcode/100. Same Tree.js
@@ -0,0 +1,64 @@
+// 100. Same Tree - Easy
+
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+ 
+
+// Example 1:
+
+
+// Input: p = [1,2,3], q = [1,2,3]
+// Output: true
+// Example 2:
+
+
+// Input: p = [1,2], q = [1,null,2]
+// Output: false
+// Example 3:
+
+
+// Input: p = [1,2,1], q = [1,1,2]
+// Output: false
+ 
+
+// Constraints:
+
+// The number of nodes in both trees is in the range [0, 100].
+// -104 <= Node.val <= 104
+
+// Solution:
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+  if (!p && !q){
+      return true;
+  }
+  //if no more p or q return true
+
+  //if p and q both exist and as long as qval == pval
+  //check the left 2
+  if (p && q && p.val === q.val){
+      return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+  }
+
+  return false
+
+};
+
+
+//need to confirm the same values
+//recursive solution check lefts, then value, then rights
