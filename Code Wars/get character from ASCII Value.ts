8957d7d6e6c3e9c0f88cd6d5bdd980339c1460ad
barento <awesomemoi@hotmail.com>
2025-04-20 14:33:32 -0700

    New kata "get character from ASCII Value"

---
+++ Code Wars/get character from ASCII Value.ts
@@ -0,0 +1,18 @@
+// 8 kyu - get character from ASCII Value
+
+// Description:
+
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// 65 --> 'A'
+// 97 --> 'a'
+// 48 --> '0
+// For ASCII table, you can refer to http://www.asciitable.com/
+
+// Solution:
+
+export function getChar(c: number): string {
+    return String.fromCharCode(c)
+  }
