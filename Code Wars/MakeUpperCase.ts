7f23b1b9a8d774836703d3dbf7591a5eec5f19f1
barento <awesomemoi@hotmail.com>
2025-06-24 23:55:50 -0400

    New ts kata "MakeUpperCase"

---
+++ Code Wars/MakeUpperCase.ts
@@ -0,0 +1,10 @@
+// 8 kyu - MakeUpperCase
+
+// Description:
+// Write a function which converts the input string to uppercase.
+
+// Solution:
+
+export function makeUpperCase(str: string): string{
+  return str.toUpperCase()
+}
