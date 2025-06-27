f18b011908414d34167deab581ab090aefbb7c81
Barento <awesomemoi@hotmail.com>
2025-06-27 10:02:12 -0400

    New ts kata "ROT13"

---
+++ Code Wars/ROT13.ts
@@ -0,0 +1,46 @@
+// 5 kyu - ROT13
+
+// Description:
+
+// How can you tell an extrovert from an introvert at NSA?
+// Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
+
+// I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it?
+// According to Wikipedia, ROT13 is frequently used to obfuscate jokes on USENET.
+
+// For this task you're only supposed to substitute characters. Not spaces, punctuation, numbers, etc.
+
+// Test examples:
+
+// "EBG13 rknzcyr." -> "ROT13 example."
+
+// "This is my first ROT13 excercise!" -> "Guvf vf zl svefg EBG13 rkprepvfr!"
+
+// Solution:
+
+export function rot13(str: string): string {
+  const lowerCase: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  const upperCase: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+  const ans: string[] = str.split('').map(x=>{
+    if (lowerCase.includes(x)){
+      let index = lowerCase.indexOf(x)
+      return index < 13 ? lowerCase[index+13] : lowerCase[index-13]
+    }
+    if (upperCase.includes(x)){
+      let index = upperCase.indexOf(x)
+      return index < 13 ? upperCase[index+13] : upperCase[index-13]
+    }
+    return x
+  })
+
+  return ans.join('');
+}
+
+// T-G 20-6
+// h-u  8-21
+// i-v 9-22
+// s-f 19-5 
+
+// need to add 13 to the place of the digit
+// if the place of the digit is 14th or later, minus 13 
+//N 14 - A 1 
