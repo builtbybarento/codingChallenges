308de3bdd0fbb84fb109f58c237d6db6b284840f
Barento <awesomemoi@hotmail.com>
2025-07-22 13:51:52 -0400

    New leetcode "199. Binary Tree Right Side View"

---
+++ Leetcode/199. Binary Tree Right Side View.ts
@@ -0,0 +1,76 @@
+// 199. Binary Tree Right Side View - Medium 
+
+// Description:
+
+// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ 
+
+// Example 1:
+
+// Input: root = [1,2,3,null,5,null,4]
+
+// Output: [1,3,4]
+
+// Explanation:
+
+
+
+// Example 2:
+
+// Input: root = [1,2,3,4,null,null,null,5]
+
+// Output: [1,3,4,5]
+
+// Explanation:
+
+
+
+// Example 3:
+
+// Input: root = [1,null,3]
+
+// Output: [1,3]
+
+// Example 4:
+
+// Input: root = []
+
+// Output: []
+
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+// Solution:
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function rightSideView(root: TreeNode | null): number[] {
+    let ans = []
+    function check(node, depth){
+        if (!node) return
+        ans[depth] = node.val
+        check(node.left, depth+1)
+        check(node.right, depth+1)
+    }
+    check(root,0)
+    return ans
+};
+//return an array of the top down view from the right
+//check right if not check left until left and right are null
+//maybe just dfs through the array and just replace the values with all the right values for every depth
