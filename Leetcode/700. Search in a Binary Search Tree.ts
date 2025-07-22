fdd67e9ec1a583c9bc6b8886d750a8003a33e2b2
Barento <awesomemoi@hotmail.com>
2025-07-22 16:35:24 -0400

    New leetcode "700. Search in a Binary Search Tree"

---
+++ Leetcode/700. Search in a Binary Search Tree.ts
@@ -0,0 +1,62 @@
+// 700. Search in a Binary Search Tree 
+
+// Description:
+
+// You are given the root of a binary search tree (BST) and an integer val.
+
+// Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [4,2,7,1,3], val = 2
+// Output: [2,1,3]
+// Example 2:
+
+
+// Input: root = [4,2,7,1,3], val = 5
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 5000].
+// 1 <= Node.val <= 107
+// root is a binary search tree.
+// 1 <= val <= 107
+
+// Solution:
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+    let ans: TreeNode = null
+    let found = false
+    function check(node){
+        if (!node) return
+        if (node.val === val){
+            ans = node
+            found = true
+        }
+        if (!found && node.left) check(node.left)
+        if (!found && node.right) check(node.right)
+    }
+    check(root)
+    return ans
+
+
+};
